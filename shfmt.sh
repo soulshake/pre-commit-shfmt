427ef1607b5c9a4a1889a89a7d2b27c1d44b365e
AJ Bowen <aj@soulshake.net>
2017-08-10 14:20:44 +0200

    Don't check deleted files

--- shfmt.sh
+++ shfmt.sh
@@ -5,7 +5,9 @@ set -o nounset  # abort on unbound variable
 set -o pipefail # capture fail exit codes in piped commands
 # set -x          # execution tracing debug messages
 
-for f in $(git diff --cached --name-only | grep .sh$); do
+echo "--> begin shfmt"
+
+for f in $(git diff --cached --diff-filter=AM --name-only | grep .sh$); do
     shfmt -l -i 4 -w "$f"
 done
 exit $?
