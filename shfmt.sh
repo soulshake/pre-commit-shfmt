81efcdb9d45e2bcb3ebb5d019c46e869258e0117
AJ Bowen <aj@soulshake.net>
2017-08-10 14:08:33 +0200

    Maybe make it work

--- shfmt.sh
+++ shfmt.sh
@@ -5,6 +5,7 @@ set -o nounset  # abort on unbound variable
 set -o pipefail # capture fail exit codes in piped commands
 # set -x          # execution tracing debug messages
 
-for file in "$@"; do
-  shfmt -l -i 4 -w "$file"
+for f in $(git diff --cached --name-only | grep .sh$); do
+    shfmt -l -i 4 -w "$f"
 done
+exit $?
